f35d5df84e4fdfcbce2d147505edaaf9b4350536
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-10-20 20:28:31 -0600

    Creación Vistas
    Vistas:
    Inicio Sesion
    Registro
    Detalles

---
+++ MoView/app/src/main/java/com/example/moview/fragments/detalles/DetallesFragment.kt
@@ -0,0 +1,13 @@
+package com.example.moview.fragments.detalles
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moview.R
+
+
+class DetallesFragment : Fragment(R.layout.fragment_detalles) {
+
+}
