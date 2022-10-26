4ef2ccfed5be6faad630c518cd74d12d0f61bf38
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-10-25 21:23:40 -0600

    Navegación

--- app/src/main/java/com/example/moview/fragments/Home/HomeFragment.kt
+++ app/src/main/java/com/example/moview/fragments/Home/HomeFragment.kt
@@ -1,7 +1,33 @@
 package com.example.moview.fragments.Home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.moview.MainActivity
 import com.example.moview.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private lateinit var bottonNav: BottomNavigationView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bottonNav = (activity as MainActivity).getToolBar()
+
+        setListeners()
+    }
+
+    private fun setListeners(){
+        bottonNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.search -> requireView().findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSearchFragment())
+                R.id.news -> requireView().findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToNewsFragment())
+                R.id.profile -> requireView().findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToProfileFragment())
+            }
+            true
+        }
+    }
+
 }
--- app/src/main/java/com/example/moview/fragments/perfilCritico/ProfileFragment.kt
+++ app/src/main/java/com/example/moview/fragments/perfilCritico/ProfileFragment.kt
@@ -1,18 +1,14 @@
-package com.example.moview.fragments.news
+package com.example.moview.fragments.perfilCritico
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.moview.MainActivity
 import com.example.moview.R
-import com.example.moview.fragments.Home.HomeFragmentDirections
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
-class NewsFragment : Fragment(R.layout.fragment_news) {
+class ProfileFragment : Fragment(R.layout.fragment_perfil) {
     private lateinit var bottonNav: BottomNavigationView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,9 +22,9 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
     private fun setListeners(){
         bottonNav.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.search -> requireView().findNavController().navigate(NewsFragmentDirections.actionNewsFragmentToSearchFragment())
-                R.id.home -> requireView().findNavController().navigate(NewsFragmentDirections.actionNewsFragmentToHomeFragment())
-                R.id.profile -> requireView().findNavController().navigate(NewsFragmentDirections.actionNewsFragmentToProfileFragment())
+                R.id.search -> requireView().findNavController().navigate(ProfileFragmentDirections.actionPerfilFragmentToSearchFragment())
+                R.id.news -> requireView().findNavController().navigate(ProfileFragmentDirections.actionPerfilFragmentToNewsFragment())
+                R.id.home -> requireView().findNavController().navigate(ProfileFragmentDirections.actionPerfilCriticoFragmentToHomeFragment())
             }
             true
         }
