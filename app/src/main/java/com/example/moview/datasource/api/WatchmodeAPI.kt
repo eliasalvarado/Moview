5b16ab49c661d4c95c1fa36fa23c4a1e63c9e062
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-11-05 16:28:28 -0600

    API

--- app/src/main/java/com/example/moview/datasource/api/WatchmodeAPI.kt
+++ app/src/main/java/com/example/moview/datasource/api/WatchmodeAPI.kt
@@ -2,6 +2,7 @@ package com.example.moview.datasource.api
 
 import com.example.moview.datasource.model.APIresponse
 import com.example.moview.datasource.model.APIresponseTitle
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,17 +11,17 @@ interface WatchmodeAPI {
     @GET("/v1/list-titles/?apiKey={apiKey}&types=movie")
     fun getMovies(
         @Path("apiKey") apiKey: String
-    ): APIresponse
+    ): Call<APIresponse>
 
     @GET("/v1/list-titles/?apiKey={apiKey}&types=tv_series")
     fun getSeries(
         @Path("apiKey") apiKey: String
-    ): APIresponse
+    ): Call<APIresponse>
 
     @GET("/v1/title/{title_id}/details/?apiKey={apiKey}&language=es")
     fun getTitle(
         @Path("title_id") idTitle: Int,
         @Path("apiKey") apiKey: String
-    ): APIresponseTitle
+    ): Call<APIresponseTitle>
 
 }
