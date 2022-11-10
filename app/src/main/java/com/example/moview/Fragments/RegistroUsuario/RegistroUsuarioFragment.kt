2ac89791a21379835bba425357848b699abb0945
DanielMorales1103 <mor21785@uvg.edu.gt>
2022-11-09 23:03:33 -0600

    Registro y login

--- app/src/main/java/com/example/moview/Fragments/RegistroUsuario/RegistroUsuarioFragment.kt
+++ app/src/main/java/com/example/moview/Fragments/RegistroUsuario/RegistroUsuarioFragment.kt
@@ -5,23 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.RadioButton
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.example.moview.R
+import com.example.moview.data.local.entity.User
+import com.example.moview.data.remote.firebase.FirebaseUserApiImpl
+import com.example.moview.data.repository.auth.user.UserRepository
+import com.example.moview.data.repository.auth.user.UserRepositoryImpl
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class RegistroUsuarioFragment : Fragment(R.layout.fragment_registro_usuario) {
     private lateinit var yesOpt: RadioButton
     private lateinit var noOpt: RadioButton
     private lateinit var createAccountButton: Button
+    private lateinit var userName: TextInputLayout
+    private lateinit var correo: TextInputLayout
+    private lateinit var contra1: TextInputLayout
+    private lateinit var contra2: TextInputLayout
+    private var esCritico = false
+    private var permitido : Boolean = false
+    private lateinit var repository : UserRepository
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        repository = UserRepositoryImpl(
+            FirebaseUserApiImpl(Firebase.firestore)
+        )
         view.apply {
             yesOpt = findViewById(R.id.radioButtonSi)
             noOpt = findViewById(R.id.radioButtonNo)
             createAccountButton = findViewById(R.id.createAccountButton)
+            userName = findViewById(R.id.ingresoUsuarioregistro)
+            correo = findViewById(R.id.ingresoCorreoRegistro)
+            contra1 = findViewById(R.id.ingresoContraRegistro)
+            contra2 = findViewById(R.id.ingresoRepetirContraRegistro)
         }
+
         setListeners()
+
     }
 
     private fun setListeners() {
@@ -29,16 +59,63 @@ class RegistroUsuarioFragment : Fragment(R.layout.fragment_registro_usuario) {
             if(noOpt.isChecked){
                 noOpt.isChecked = false
             }
+            esCritico=true
         }
         noOpt.setOnClickListener {
             if(yesOpt.isChecked){
                 yesOpt.isChecked = false
             }
+            esCritico=false
         }
         createAccountButton.setOnClickListener {
-            requireView().findNavController().navigate(
-                RegistroUsuarioFragmentDirections.actionRegistroUsuarioFragmentToHomeFragment()
-            )
+            CoroutineScope(Dispatchers.Main).launch {
+                crearUsuario()
+            }
+
         }
     }
+
+    private fun crearUsuario() {
+        val usuario = userName.editText!!.text.toString()
+        val email = correo.editText!!.text.toString()
+        val pasword = contra1.editText!!.text.toString()
+        val pasword2 = contra2.editText!!.text.toString()
+        if (usuario == "" || email == "" || pasword== "" || pasword2==""){
+            Toast.makeText(activity,"Por favor llene todos los campos",Toast.LENGTH_LONG).show()
+        }else{
+            if(pasword.compareTo(pasword2) == 0){
+                lifecycleScope.launch(Dispatchers.IO){
+                    val comprobacionUser = repository.getUser(usuario)
+                    val comprobacionEmail = repository.getUserByEmail(email)
+                    if(comprobacionUser!=null || comprobacionEmail!=null){
+                        permitido =true
+                        repository.createUser(
+                            user = User(
+                                user = usuario,
+                                email = email,
+                                pasword = pasword,
+                                critico = esCritico,
+                                perfil = ""
+                            )
+                        )
+                    }else{
+                        permitido=false
+                    }
+                }
+                if(permitido){
+                    requireView().findNavController().navigate(
+                        RegistroUsuarioFragmentDirections.actionRegistroUsuarioFragmentToHomeFragment()
+                    )
+                }else{
+                    Toast.makeText(activity,"El nombre de usuario o correo está en uso",Toast.LENGTH_LONG).show()
+                }
+
+            }else{
+                Toast.makeText(activity,"Las contraseñas no coinciden",Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
+
 }
