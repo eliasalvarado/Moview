1297c0fe83b2ee7c3db893bce577ff1282f573b0
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-11-19 17:09:31 -0600

    Cambios

--- app/src/main/java/com/example/moview/Fragments/Home/HomeFragment.kt
+++ app/src/main/java/com/example/moview/Fragments/Home/HomeFragment.kt
@@ -12,7 +12,15 @@ import com.example.moview.Fragments.Adapters.Home.model.CategoryClass
 import com.example.moview.Fragments.Adapters.Home.model.CategoryItem
 import com.example.moview.MainActivity
 import com.example.moview.R
+import com.example.moview.datasource.api.RetrofitInstance
+import com.example.moview.datasource.model.APIresponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment: Fragment(R.layout.fragment_home), CategoryItemAdapter.RecyclerViewItemClickHandler {
     private lateinit var bottonNav: BottomNavigationView
@@ -20,6 +28,8 @@ class HomeFragment: Fragment(R.layout.fragment_home), CategoryItemAdapter.Recycl
     private lateinit var categoryRecyclerAdapter: CategoryRecyclerAdapter
     private lateinit var list: MutableList<CategoryClass>
     private lateinit var secondaryList: MutableList<CategoryItem>
+
+    private val key = "NJvzEz4BNnsCPqmLwIiNtXjuW1jfLW4hTYYr0Prq"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -29,16 +39,18 @@ class HomeFragment: Fragment(R.layout.fragment_home), CategoryItemAdapter.Recycl
         instanceData()
         setListeners()
         setCategoryRecycler(list)
+        cargarAPI()
+
     }
 
     private fun instanceData() {
         val itemList: MutableList<CategoryItem> = ArrayList()
-        itemList.add(CategoryItem("Breaking Bad", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
-        itemList.add(CategoryItem("Breaking Bad", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
-        itemList.add(CategoryItem("Breaking Bad", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
-        itemList.add(CategoryItem("Breaking Bad", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
-        itemList.add(CategoryItem("Breaking Bad", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
-        itemList.add(CategoryItem("Breaking Bad", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
+        itemList.add(CategoryItem("Breaking Bad 1", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
+        itemList.add(CategoryItem("Breaking Bad 2", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
+        itemList.add(CategoryItem("Breaking Bad 3", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
+        itemList.add(CategoryItem("Breaking Bad 4", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
+        itemList.add(CategoryItem("Breaking Bad 5", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
+        itemList.add(CategoryItem("Breaking Bad 6", "https://cdn.watchmode.com/posters/03173903_poster_w185.jpg"))
         val listOfAll: MutableList<CategoryClass> = ArrayList()
         listOfAll.add(CategoryClass("Romance", "Series", itemList))
         listOfAll.add(CategoryClass("Acción", "Series", itemList))
@@ -78,6 +90,23 @@ class HomeFragment: Fragment(R.layout.fragment_home), CategoryItemAdapter.Recycl
 
     override fun onItemClicked(item: CategoryItem) {
         println("Se ha presionado el item: "+ item.toString())
+    }
+
+    private fun cargarAPI() {
+
+        RetrofitInstance.api.getMovies().enqueue(object: Callback<APIresponse> {
+            override fun onResponse(call: Call<APIresponse>, response: Response<APIresponse>) {
+                if (response.isSuccessful && response.body() != null) {
+                    var moviesListAPI = response.body()!!.titles
+                    println(response.body())
+                }
+            }
+
+            override fun onFailure(call: Call<APIresponse>, t: Throwable) {
+                println("Error")
+            }
+
+        })
     }
 
 }
--- app/src/main/java/com/example/moview/datasource/api/WatchmodeAPI.kt
+++ app/src/main/java/com/example/moview/datasource/api/WatchmodeAPI.kt
@@ -5,23 +5,24 @@ import com.example.moview.datasource.model.APIresponseTitle
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface WatchmodeAPI {
 
-    @GET("/v1/list-titles/?apiKey={apiKey}&types=movie")
+    @GET("/v1/list-titles/?apiKey=NJvzEz4BNnsCPqmLwIiNtXjuW1jfLW4hTYYr0Prq&types=movie")
     fun getMovies(
-        @Path("apiKey") apiKey: String
+
     ): Call<APIresponse>
 
     @GET("/v1/list-titles/?apiKey={apiKey}&types=tv_series")
     fun getSeries(
-        @Path("apiKey") apiKey: String
+        @Query("apiKey") apiKey: String
     ): Call<APIresponse>
 
     @GET("/v1/title/{title_id}/details/?apiKey={apiKey}&language=es")
     fun getTitle(
-        @Path("title_id") idTitle: Int,
-        @Path("apiKey") apiKey: String
+        @Query("title_id") idTitle: Int,
+        @Query("apiKey") apiKey: String
     ): Call<APIresponseTitle>
 
 }
